619973ced19f5910dafe9fc65c650c5e28d192e8
Matti Airas <mairas@iki.fi>
2021-11-20 14:44:01 +0200

    Update the examples

--- examples/minimal/src/main.cpp
+++ examples/minimal/src/main.cpp
@@ -1,27 +1,33 @@
 #include <Arduino.h>
 #include <ReactESP.h>
 
-extern "C" {
-#include "user_interface.h"
-}
-
 #define LED_PIN 2
 
 int led_state = 0;
 
-ReactESP app([] () {
+ReactESP app;
+
+void setup() {
   Serial.begin(115200);
   Serial.println("Starting");
   pinMode(LED_PIN, OUTPUT);
   
   Serial.println("Setting up timed reactions");
+
+  // toggle LED every 400 ms
   app.onRepeat(400, [] () {
     led_state = !led_state;
     digitalWrite(LED_PIN, led_state);
   });
+
+  // Additionally, toggle LED every 1020 ms.
+  // This adds an irregularity to the LED blink pattern.
   app.onRepeat(1020, [] () {
     led_state = !led_state;
     digitalWrite(LED_PIN, led_state);
-  });
-  
-});
+  });  
+}
+
+void loop() {
+  app.tick();
+}
